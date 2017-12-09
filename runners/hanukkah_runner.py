c405bbc173488d8a3348993bebc615cbea7340a9
Omer Kapach <omerkap@gmail.com>
2017-12-09 10:41:47 +0200

    [Omer] Sovsov is working and emulating with points and scale function

--- runners/hanukkah_runner.py
+++ runners/hanukkah_runner.py
@@ -1,14 +1,17 @@
 from SR_Board.sr_driver import SRDriver
+from screen_usages.sovsov import Sovsov
 from time import sleep
 import os
+import matplotlib.pyplot as plt
+
 
 class HanukkahRunner(object):
     def __init__(self):
-        self.phi = 0;
-        self.dtheta_self = 0
-        self.dtheta_inclined = 0
+        self.phi = 40
+        self.dtheta_self = 5
+        self.dtheta_inclined = 10
         self.shift_origin = (0,0,0)
-        self.delay = 0.1
+        self.delay = 0.01
         self.sr_driver = SRDriver(board_num_of_regs=56,
                                  num_of_boards=4,
                                  clk_pin=11,
@@ -16,13 +19,22 @@ class HanukkahRunner(object):
                                  data_pin=13,
                                  index_map_file=os.path.join('..', 'SR_Board', 'index_map.csv'),
                                  is_simulated=True)
-        self.sovsov = sovsov(phi = self.phi,
+        self.sovsov = Sovsov(phi = self.phi,
                              dtheta_self = self.dtheta_self,
                              dtheta_inclined = self.dtheta_inclined,
                              shift_origin = self.shift_origin)
 
-    def run(self):
+    def start(self):
+        plt.ion()
+        f = plt.figure()
         while True:
             sovsov_data = self.sovsov.get_next_step()
             self.sr_driver.draw(sovsov_data)
+            f.clf()
+            plt.imshow(sovsov_data, cmap='hot')
+            f.canvas.draw()
             sleep(self.delay)
+
+if __name__ == '__main__':
+    hanukkah_runner = HanukkahRunner()
+    hanukkah_runner.start()
--- screen_usages/sovsov.py
+++ screen_usages/sovsov.py
@@ -1,36 +1,40 @@
 import numpy as np
+from abstract_screen_usage import AbstractScreenUsage
+import matplotlib.pyplot as plt
 
-class Sovsov(object):
+
+class Sovsov(AbstractScreenUsage):
+    SCALE = 5
     BOARD_SIZE = [17,11]
-    ORIGIN = [8,5]
+    DRAW_ORIGIN = [8, 5]
+
     def __init__(self, phi, dtheta_self, dtheta_inclined, shift_origin):
-        self.a= ([-1, 1, 1],
+        self.a= np.asarray(([-1., 1., 1.],
             [-1, -1, 1],
             [1, -1, 1],
             [1, 1, 1],
             [-1,1,-1],
             [-1,-1,-1],
-            [1,-1-1],
-            [1,1,-1])
-        connecting_points = ([0,1],[1,2],[2,3],[3,0],
+            [1,-1,-1],
+            [1,1,-1]))
+        self.a *= 2 * self.SCALE
+        print self.a
+        self.connecting_points = ([0,1],[1,2],[2,3],[3,0],
                              [4,1],[5,6],[6,7],[7,4],
                              [0,4],[1,5],[2,6],[3,7])
         self.shift(shift_origin[0], shift_origin[1], shift_origin[2])
         self.a = np.transpose(self.a)
         self.phi = phi
         self.theta_inclined = 0
-        self.dtheta_self = dtheta_self
-        self.dtheta_inclined = dtheta_inclined
+        self.dtheta_self = np.deg2rad(dtheta_self)
+        self.dtheta_inclined = np.deg2rad(dtheta_inclined)
         self.spin_y(self.phi)
 
     # screen horizontal is x, vertical is y and depth is z
     def shift(self,x,y,z):
-        shift_mat = np.zeros(shape=(8,3))
-        for point in shift_mat:
-            point[0] = x
-            point[1] = y
-            point[2] = z
-        self.a += shift_mat
+        self.a[:, 0] += x
+        self.a[:, 1] += y
+        self.a[:, 2] += z
 
     def spin_x(self,theta):
         spin = ([1,0,0],[0,np.cos(theta),-np.sin(theta)],[0,np.sin(theta),np.cos(theta)])
@@ -51,18 +55,14 @@ class Sovsov(object):
         self.spin_y(self.phi)
         self.theta_inclined = self.theta_inclined + self.dtheta_inclined
         self.spin_z(self.theta_inclined)
-        drawable = self.get_drawable()
-        return self.a
+        return self.get_drawable()
 
     def get_drawable(self):
-        projected_a = self.a[0:1]
+        projected_a = self.a[:2]
         projected_a = np.transpose(projected_a)
-        # x_region = [0, max(projected_a[0])-min(projected_a[0])]
-        # y_region = [0, max(projected_a[1])-min(projected_a[1])]
-        # np.zeros(shape=(max(projected_a[0])-min(projected_a[0]),
-        #                 max(projected_a[1])-min(projected_a[1])))
-        drawable = np.zeros((self.BOARD_SIZE[0],self.BOARD_SIZE[1]))
+        drawable = np.zeros((self.BOARD_SIZE[0]*self.SCALE,self.BOARD_SIZE[1]*self.SCALE))
         for point in projected_a:
-            drawable[point[0] + self.ORIGIN[0],point[1] + self.ORIGIN[1]] = 1
-
+            drawable[round(point[0] + self.DRAW_ORIGIN[0]*self.SCALE),
+                     round(point[1] + self.DRAW_ORIGIN[1]*self.SCALE)] = 1
+        return drawable
 
