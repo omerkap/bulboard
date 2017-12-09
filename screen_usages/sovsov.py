ff5969ae400c714f68e0c0cf3587bbfdcf2e4483
Omer Kapach <omerkap@gmail.com>
2017-12-09 11:29:12 +0200

    [Omer] Added line rendering option to sovsov (get_drawable function)

--- screen_usages/sovsov.py
+++ screen_usages/sovsov.py
@@ -1,10 +1,11 @@
 import numpy as np
+import math
 from abstract_screen_usage import AbstractScreenUsage
 import matplotlib.pyplot as plt
 
 
 class Sovsov(AbstractScreenUsage):
-    SCALE = 5
+    SCALE = 10
     BOARD_SIZE = [17,11]
     DRAW_ORIGIN = [8, 5]
 
@@ -17,11 +18,11 @@ class Sovsov(AbstractScreenUsage):
             [-1,-1,-1],
             [1,-1,-1],
             [1,1,-1]))
-        self.a *= 2 * self.SCALE
+        self.a *= 3 * self.SCALE
         print self.a
-        self.connecting_points = ([0,1],[1,2],[2,3],[3,0],
-                             [4,1],[5,6],[6,7],[7,4],
-                             [0,4],[1,5],[2,6],[3,7])
+        self.lines = ([0,1],[1,2],[2,3],[3,0],
+                        [4,5],[5,6],[6,7],[7,4],
+                        [0,4],[1,5],[2,6],[3,7])
         self.shift(shift_origin[0], shift_origin[1], shift_origin[2])
         self.a = np.transpose(self.a)
         self.phi = phi
@@ -55,14 +56,27 @@ class Sovsov(AbstractScreenUsage):
         self.spin_y(self.phi)
         self.theta_inclined = self.theta_inclined + self.dtheta_inclined
         self.spin_z(self.theta_inclined)
-        return self.get_drawable()
+        return self.get_drawable(render_lines= 1)
 
-    def get_drawable(self):
+    def get_drawable(self, render_lines):
         projected_a = self.a[:2]
         projected_a = np.transpose(projected_a)
         drawable = np.zeros((self.BOARD_SIZE[0]*self.SCALE,self.BOARD_SIZE[1]*self.SCALE))
-        for point in projected_a:
-            drawable[round(point[0] + self.DRAW_ORIGIN[0]*self.SCALE),
-                     round(point[1] + self.DRAW_ORIGIN[1]*self.SCALE)] = 1
+        if (render_lines == 0):
+            for point in projected_a:
+                drawable[round(point[0] + self.DRAW_ORIGIN[0]*self.SCALE),
+                         round(point[1] + self.DRAW_ORIGIN[1]*self.SCALE)] = 1
+        else:
+            res_enhance = 2
+            for line in self.lines:
+                p0 = projected_a[line[0]]
+                p1 = projected_a[line[1]]
+                vector = p1 - p0
+                vector_length = math.sqrt(vector[0]**2 + vector[1]**2)
+                vector /= vector_length #normalized
+                for step in range(0,int(math.floor(vector_length*res_enhance))):
+                    next_p = p0 + vector/res_enhance*step
+                    drawable[self.DRAW_ORIGIN[0] * self.SCALE + next_p[0],
+                             self.DRAW_ORIGIN[1] * self.SCALE + next_p[1]] = 1
         return drawable
 
